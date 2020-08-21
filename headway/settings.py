63ff4da1995552692efbcbf264a26b2bc4141bdd
Romez <Romez1990@gmail.com>
2020-08-22 00:05:59 +0300

    feat: install cors headers

--- headway/settings.py
+++ headway/settings.py
@@ -28,6 +28,9 @@ DEBUG = env_bool('DEBUG')
 
 ALLOWED_HOSTS = [] if DEBUG else [env('HOST')]
 
+CORS_ORIGIN_ALLOW_ALL = DEBUG
+CORS_ORIGIN_WHITELIST = [env('CLIENT_ORIGIN')]
+
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'headway.urls'

615003738d25b44122d0d96c6d8a586a3609363c
Romez <Romez1990@gmail.com>
2020-08-22 09:54:46 +0300

    feat(courses.models): create user, case, course and course progress models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    CharField,
+    IntegerField,
+    TextChoices,
+    ForeignKey,
+    CASCADE,
+)
 
-# Create your models here.
+
+class User(Model):
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100)
+    age = IntegerField()
+    gender = TextChoices('male', 'female')
+    subdivision = CharField(max_length=100)
+    status = CharField(max_length=100)
+
+
+class Case(Model):
+    name = CharField(max_length=255)
+
+
+class Course(Model):
+    name = CharField(max_length=255)
+
+
+class CourseProgress(Model):
+    user = ForeignKey(User, CASCADE)
+    course = ForeignKey(Course, CASCADE)
+    percent_complete = IntegerField()

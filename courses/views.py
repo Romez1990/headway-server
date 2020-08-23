589e70ba8926aa7c9e8adb73af900b2bdc47b761
Romez <Romez1990@gmail.com>
2020-08-23 06:26:12 +0300

    feat(courses.view): create user, case, course and course progress view sets

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .serializers import (
+    UserSerializer,
+    CaseSerializer,
+    CourseSerializer,
+    CourseProgressSerializer,
+)
+from .models import (
+    User,
+    Case,
+    Course,
+    CourseProgress,
+)
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects
+
+
+class CaseViewSet(ModelViewSet):
+    serializer_class = CaseSerializer
+    queryset = Case.objects
+
+
+class CourseViewSet(ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects
+
+
+class CourseProgressViewSet(ModelViewSet):
+    serializer_class = CourseProgressSerializer
+    queryset = CourseProgress.objects

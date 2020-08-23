7c45d51d42f19ea31265fea2d65f039373301023
Romez <Romez1990@gmail.com>
2020-08-23 06:31:04 +0300

    feat(courses.urls): add user, case, course and course progress to the router

--- courses/urls.py
+++ courses/urls.py
@@ -1,4 +1,19 @@
 from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import (
+    UserViewSet,
+    CaseViewSet,
+    CourseViewSet,
+    CourseProgressViewSet,
+)
+
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('cases', CaseViewSet, basename='cases')
+router.register('courses', CourseViewSet, basename='courses')
+router.register('course-progresses', CourseProgressViewSet, basename='course-progresses')
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
